4fe1f6414e3ad8e597f545cd597191d6153f28e8
Paul Wilson <pswilson@4thoughttech.com>
2017-09-15 17:09:25 -0400

    initial

---
+++ README.md
@@ -0,0 +1,25 @@
+# Meetup Utilities
+
+Meetup attendance aggregator
+
+... or at least the basic start of one
+
+### Configuration
+
+You'll need a a few environment variables set for this to run.
+
+MEETUP_API_BASE = <the_meetup_api_url>
+MEETUP_API_KEY = <your_api_key>
+
+### Things to do (in no particular order or structure):
+
+* create linked meetup events (common events across meetup groups)
+  * consolidate attendance info across linked events
+* structure as a service
+* deploy as a service
+* return some consumable format other than text
+  * or have an optional return type
+* pull meetup info from a db
+  * list of meetup groups
+  * group access info?
+    * probably better to have a single user with access to all groups and get the api key from an environment var
---
+++ attendance.py
@@ -0,0 +1,53 @@
+import requests
+
+#apiBase = 'https://api.meetup.com'
+# NOTE: Don't commit with a key set
+#apiKey = 'Your_API_Key_Here'
+
+apiBase = os.environ['MEETUP_API_BASE']
+apiKey = os.environ['MEETUP_API_KEY']
+
+# API calls using an api key rewuire ... ?key=<api_key>&sign=true
+authParams = {'key': apiKey, 'sign': 'true'}
+
+groupUrlNames = [
+   'Frederick-Startup-Community',
+   'FredWebTech'
+]
+
+eventsApiPath = 'events'
+rsvpsApiPath = 'rsvps'
+
+# GET: apiBase + '/' <group_url_name> + '/events'
+#    array of event objects
+# GET: apiBase + '/' <group_url_name> + '/events/' + <event_id>
+#    event object
+# GET: apiBase + '/' <group_url_name> + '/events/' + <event_id> + '/rsvps'
+#    array of rsvp objects
+
+def getEvents(groupUrl):
+    request_url = '{}/{}/{}'.format(apiBase, groupUrl, eventsApiPath)
+    print("Getting events from: {}".format(request_url))
+    r_events = requests.get(request_url, params = authParams)
+    print("... got status: {}".format(r_events.status_code))
+    return r_events.json()
+
+def getAttendees(groupUrl, eventId):
+    request_url = '{}/{}/{}/{}/{}'.format(apiBase, groupUrl, eventsApiPath, eventId, rsvpsApiPath)
+    print("Getting event attendees from: {}".format(request_url))
+    r_attendees = requests.get(request_url, params = authParams)
+    print("... got status: {}".format(r_attendees.status_code))
+    return r_attendees.json()
+
+def getAttendanceForGroup(groupUrl):
+    print('Events for {} ...'.format(groupUrl))
+    events = getEvents(groupUrl)
+    for event in events:
+        print('{} ({}): {}'.format(event['name'], event['id'], event['yes_rsvp_count']))
+        attendees = getAttendees(groupUrl, event['id'])
+        for attendee in attendees:
+            # print(attendee)
+            print('    {} ({}): {}'.format(attendee['member']['name'], attendee['member']['id'], attendee['response']))
+
+for groupUrl in groupUrlNames:
+    getAttendanceForGroup(groupUrl)
